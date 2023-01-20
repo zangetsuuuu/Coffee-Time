2d336c9d4beb292f2962d9c29bb90bbda65ebb0b
Rafif Athallah <rafifathallah99@gmail.com>
2023-01-20 19:50:03 +0700

    Membuat website CoffeTime

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Remove toggle
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
